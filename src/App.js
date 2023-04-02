8fa0e3b48079d202632a2df8f26f4e9a6e103a33
Rinzler2603 <97046922+Rinzler2603@users.noreply.github.com>
2023-04-02 13:45:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// import About from './comp/About';
+import Navbar from './comp/Navbar';
+import Textform from './comp/Textform';
+import Alert from './comp/Alert';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+
+// let name="Hardik";
 
 function App() {
+  const [mode,setMode]=useState('light');
+  const [alert, setAlert] = useState(null);
+  
+
+  const showAlert=(message, type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  };
+
+  const toggleMode=()=>{
+    if(mode==="light"){
+      setMode("dark");
+      document.body.style.backgroundColor='rgb(23 33 88)';
+      showAlert("dark Mode has been enabled","success")
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor='white';
+      showAlert("light Mode has been enabled","success")
+    }
+  }
+  
+
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/About",
+  //     element: <About />,
+  //   },
+  //   {
+  //     path:"/",
+  //     element: <Textform showAlert={showAlert} heading="Enter the text to analyse" mode={mode}/>
+  //   }
+  // ]);
+    
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert}/>
+    <br /><br />
+    <div className="container">
+    <br />
+    <Textform showAlert={showAlert} heading="Enter the text to analyse" mode={mode}/>
+    {/* <RouterProvider router={router} /> */}
     </div>
+   
+    </>
   );
 }
 
---
+++ src/comp/Textform.js
@@ -0,0 +1,41 @@
+import React,{useState} from 'react'
+
+
+export default function Textform(props) {
+    const handleUpClick=()=>{
+        let newText=text.toUpperCase();
+        setText(newText);
+        props.showAlert("Your Text has been converted to uppercase","success");
+    }
+    const handleLoClick=()=>{
+        let newText=text.toLowerCase();
+        setText(newText);
+        props.showAlert("Your Text has been converted to lowercase","success");
+    }
+    const handleOnChange=(event)=>{
+        setText(event.target.value);
+    }
+    const [text,setText]=useState("");
+
+    return (
+        <>
+        <div>
+            <h1 style={{color:props.mode==='light'?'black':'white'}}>{props.heading}</h1>
+            <div className="mb-3 my-3">
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="floatingTextarea2" style={{height: 100}}></textarea>
+
+                <button className="btn btn-primary my-2 "onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary my-2 mx-2"onClick={handleLoClick}>Convert to Lowercase</button>
+            </div>
+
+        </div>
+        <div className="container " style={{color:props.mode==='light'?'black':'white'}}>
+            <h2 style={{color:props.mode==='light'?'black':'white'}}>Your text summary</h2>
+            <p >{text.split(' ').length-1} words and {text.length} characters</p>
+            <br />
+            <h3>Text Preview</h3>
+            <p>{text.length>0?text:"Type Something to Preview Here"}</p>
+        </div>
+        </>
+    )
+}
